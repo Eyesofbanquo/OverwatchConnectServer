c5e98ad7e78e73e0c877b0391877ee26ec825a54
Eyesofbanquo <mls103020@utdallas.edu>
2016-06-18 13:19:15 -0500

    added in the BCEncrypt

--- app.rb
+++ app.rb
@@ -17,9 +17,11 @@ class Lobby
 	include DataMapper::Resource
 	property :id, Serial, :key => true
 	property :username, Text
+	property :password, BCryptHash
 	property :platform, Text
 	property :region, Text
 	property :groupsize, Integer
+	property :groupid, Text
 	property :udid, Text
 end
 
@@ -40,9 +42,26 @@ class MyApp<Sinatra::Base
 	get '/' do
 		"Hello World!"
 	end
-	post '/create-lobby' do
-		@lobby = Lobby.new(:username => params[:username], :platform => params[:platform], :region => params[:region], :groupsize => params[:groupSize], :udid => params[:udid])
+	get '/user' do
+		@lobby = Lobby.get(:username => params[:username], :password => params[:password])
+		v = @lobby.collect{|item| {:username => item.username, :password => item.password}}
+		v.to_json
+	end
+	#parameter requirements
+	#username | password | udid
+	post '/login' do
+		@lobby = Lobby.new(:username => params[:username], :password => params[:password], :udid => params[:udid])
 		@lobby.save if Lobby.count(:username=>"#{params[:username].to_str}") == 0
+	end
+	#Parameter requirements
+	#username | region | platform | groupsize
+	put '/create-lobby' do
+		@lobby = Lobby.get(:username => params[:username])
+		@lobby.update(:region => params[:region], :platform => params[:platform], :groupsize => params[:groupSize], :groupid => SecureRandom.hex)
+		@lobby
+#	post '/create-lobby' do
+	#	@lobby = Lobby.new(:username => params[:username], :platform => params[:platform], :region => params[:region], :groupsize => params[:groupSize], :groupid => SecureRandom.hex, :udid => params[:udid])
+		#@lobby.save if Lobby.count(:username=>"#{params[:username].to_str}") == 0
 		#username = params[:username].to_str
 		#region = params[:region].to_str
 		#platform = params[:platform].to_str
@@ -50,7 +69,7 @@ class MyApp<Sinatra::Base
 		#lobby = Lobby.new(username, platform, region, group_size)
 		#@regions[region][platform].push(lobby)
 		
-	end
+	#end
 	get '/groups' do
 		#platform = params[:platform].to_str
 		#filtered_group = @regions["us"]
