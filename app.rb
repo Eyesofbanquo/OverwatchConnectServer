272efbcb14798f19636c3509f45e2f5de25a6387
Eyesofbanquo <mls103020@utdallas.edu>
2016-06-19 22:35:05 -0500

    we in there now?

--- app.rb
+++ app.rb
@@ -99,12 +99,12 @@ class MyApp<Sinatra::Base
 
 		@lobby = Lobby.new(:username => params[:username], :password => params[:password], :udid => params[:udid], :region => params[:region], :groupid => SecureRandom.hex, :platform => params[:platform], :owner => false)
 		if @lobby.password == "yusuke"
-			v.replace({:status => "error"})
+		@lobby.save if Lobby.count(:udid=>"#{params[:udid].to_str}") == 0			
+
+			
 		end
 		if @lobby.password != "yusuke"
-			@lobby.save if Lobby.count(:udid=>"#{params[:udid].to_str}") == 0
-			v["status"] = "success"
-			
+			v.replace({:status => "error"})
 		end
 		#puts user
 		
