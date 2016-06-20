3674aca213a6fcf9e1d8f86850885eb5bb797576
Eyesofbanquo <mls103020@utdallas.edu>
2016-06-19 20:45:59 -0500

    fix

--- app.rb
+++ app.rb
@@ -100,9 +100,9 @@ class MyApp<Sinatra::Base
 			
 		@lobby = Lobby.new(:username => params[:username], :password => params[:password], :udid => params[:udid], :region => params[:region], :groupid => SecureRandom.hex, :platform => params[:platform])
 		@lobby.save if Lobby.count(:udid=>"#{params[:udid].to_str}") == 0
-		lobby2 = Lobby.first(:udid => params[:udid])
+		@lobby2 = Lobby.first(:udid => params[:udid])
 		#This means the user was found so now must check password
-		if lobby2["password"] == params[:password]
+		if @lobby2["password"] == params[:password]
 			v["status"] = "success!"
 		else 
 			v["status"] = "error"
@@ -113,7 +113,7 @@ class MyApp<Sinatra::Base
 	#username | region | platform | groupsize
 	post '/create-lobby' do
 		lobby = Lobby.all(:username => params[:username])
-		Lobby.first_or_create(:username => params[:username]).update(:platform => params[:platform], :groupsize => params[:groupSize],:region => params[:region], :groupid => SecureRandom.hex, :udid => params[:udid], :owner => true)
+		Lobby.first_or_create(:username => params[:username]).update(:password => params[:password], :platform => params[:platform], :groupsize => params[:groupSize],:region => params[:region], :groupid => SecureRandom.hex, :udid => params[:udid], :owner => true)
 		#@lobby
 #	post '/create-lobby' do
 	#	@lobby = Lobby.new(:username => params[:username], :platform => params[:platform], :region => params[:region], :groupsize => params[:groupSize], :groupid => SecureRandom.hex, :udid => params[:udid])
