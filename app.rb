1abc1ea54b3c4852e3c210ecbb3e3a1e53b6e113
Eyesofbanquo <mls103020@utdallas.edu>
2016-06-19 22:58:39 -0500

    trying to use single instead of double quotations

--- app.rb
+++ app.rb
@@ -97,7 +97,7 @@ class MyApp<Sinatra::Base
 	post '/login' do
 		v = {:status => ""}
 
-		@lobby = Lobby.new(:username => params[:username], :password => params[:password], :udid => params[:udid], :region => params[:region], :groupid => SecureRandom.hex, :platform => params[:platform], :owner => "false")
+		@lobby = Lobby.new(:username => params[:username], :password => params[:password], :udid => params[:udid], :region => params[:region], :groupid => SecureRandom.hex, :platform => params[:platform], :owner => 'false')
 		if Lobby.first(:udid => params[:udid]) != nil
 			user = Lobby.first(:udid => params[:udid])
 			
@@ -146,7 +146,7 @@ class MyApp<Sinatra::Base
 	#username | region | platform | groupsize
 	post '/create-lobby' do
 		lobby = Lobby.all(:username => params[:username])
-		Lobby.first_or_create(:username => params[:username]).update(:password => params[:password], :platform => params[:platform], :groupsize => params[:groupSize],:region => params[:region], :groupid => SecureRandom.hex, :udid => params[:udid], :owner => "true")
+		Lobby.first_or_create(:username => params[:username]).update(:password => params[:password], :platform => params[:platform], :groupsize => params[:groupSize],:region => params[:region], :groupid => SecureRandom.hex, :udid => params[:udid], :owner => 'true')
 		#@lobby
 #	post '/create-lobby' do
 	#	@lobby = Lobby.new(:username => params[:username], :platform => params[:platform], :region => params[:region], :groupsize => params[:groupSize], :groupid => SecureRandom.hex, :udid => params[:udid])
@@ -162,18 +162,19 @@ class MyApp<Sinatra::Base
 	post '/join-lobby' do
 		username = params[:username]
 		groupid = params[:groupid]
-		
+		#owner = "false"
 		#This should get the owner of the group
 		lobby = Lobby.first(:groupid => groupid)
 		player = Lobby.first(:username => username)
-		player.update(:groupid => lobby["groupid"], :owner => "false")
+		player.update(:groupid => lobby["groupid"], :owner => 'false')
 	end
 	get '/groups' do
 		#platform = params[:platform].to_str
 		#filtered_group = @regions["us"]
 		platform = params[:platform]
 		region = params[:region]
-		lobbies = Lobby.all(:platform => platform, :region => region, :owner => "true")
+		#owner = "true"
+		lobbies = Lobby.all(:platform => platform, :region => region, :owner => 'true')
 		v = lobbies.collect{|item| {:username => item.username, :udid => item.udid, :groupSize => item.groupsize, :groupid => item.groupid}}
 		#"#{lobbies.get(1)["username"]}"
 		v.to_json
