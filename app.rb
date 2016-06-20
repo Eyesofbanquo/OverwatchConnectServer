3418671823d60cbdf628cf5649c4a8de5c2585d7
Eyesofbanquo <mls103020@utdallas.edu>
2016-06-19 20:54:52 -0500

    pls2

--- app.rb
+++ app.rb
@@ -100,7 +100,7 @@ class MyApp<Sinatra::Base
 			
 		@lobby = Lobby.new(:username => params[:username], :password => params[:password], :udid => params[:udid], :region => params[:region], :groupid => SecureRandom.hex, :platform => params[:platform])
 		@lobby.save if Lobby.count(:udid=>"#{params[:udid].to_str}") == 0
-		@lobby2 = Lobby.all(:udid => params[:udid])
+		@lobby2 = Lobby.first(:udid => params[:udid])
 		#This means the user was found so now must check password
 		password = @lobby2["password"]
 		if password == params[:password]
